c1b55501aa43ebf8c3918b9bacc5539800f49682
waqaskaleem007 <kaleemullah2932@gmail.com>
2020-04-14 09:09:22 +0500

    chaman dryfruit application

--- app/src/main/java/com/example/chamandryfruits/SignInFragment.java
+++ app/src/main/java/com/example/chamandryfruits/SignInFragment.java
@@ -1,13 +1,21 @@
 package com.example.chamandryfruits;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+
+import java.util.Objects;
 
 
 /**
@@ -16,7 +24,7 @@ import android.widget.TextView;
 public class SignInFragment extends Fragment {
 
     private TextView dontHaveAnAccount;
-
+    private FrameLayout parentFrameLayout;
 
     public SignInFragment() {
         // Required empty public constructor
@@ -25,12 +33,37 @@ public class SignInFragment extends Fragment {
 
 
 
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
         dontHaveAnAccount = view.findViewById(R.id.sign_in_newuser);
+        parentFrameLayout = Objects.requireNonNull(getActivity()).findViewById(R.id.register_framelayout);
         return view;
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        dontHaveAnAccount.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                setFragment(new SignUpFragment());
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_right,R.anim.slideout_from_left);
+        fragmentTransaction.replace(parentFrameLayout.getId(),fragment);
+        fragmentTransaction.commit();
+    }
+
 }
--- app/src/main/java/com/example/chamandryfruits/SignUpFragment.java
+++ app/src/main/java/com/example/chamandryfruits/SignUpFragment.java
@@ -1,12 +1,21 @@
 package com.example.chamandryfruits;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 
 /**
@@ -14,15 +23,43 @@ import android.view.ViewGroup;
  */
 public class SignUpFragment extends Fragment {
 
+    private TextView alreadyHaveAnAccount;
+    private FrameLayout parentFrameLayout;
+
     public SignUpFragment() {
         // Required empty public constructor
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view =  inflater.inflate(R.layout.fragment_sign_up, container, false);
+        alreadyHaveAnAccount = view.findViewById(R.id.sign_up_signin);
+        parentFrameLayout = Objects.requireNonNull(getActivity()).findViewById(R.id.register_framelayout);
+        return view;
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        alreadyHaveAnAccount.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                setFragment(new SignInFragment());
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_left,R.anim.slideout_from_right);
+        fragmentTransaction.replace(parentFrameLayout.getId(),fragment);
+        fragmentTransaction.commit();
     }
 }
