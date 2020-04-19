bf95fff05b49d381f3b61ac9a8ee035fb5dd8765
waqaskaleem007 <kaleemullah2932@gmail.com>
2020-04-19 20:21:06 +0500

    Navigation drawer

--- app/src/main/java/com/example/chamandryfruits/Home.java
+++ app/src/main/java/com/example/chamandryfruits/Home.java
@@ -1,21 +1,130 @@
 package com.example.chamandryfruits;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
 
-import static android.os.SystemClock.sleep;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
 
 public class Home extends AppCompatActivity {
 
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_activity);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        int id = menuItem.getItemId();
+                        if(id == R.id.nav_my_mall){
+                            Intent intent = new Intent(Home.this,RegisterActivity.class);
+                            startActivity(intent);
+                            return true;
+                        }
+                        else if(id == R.id.nav_my_account){
+                            return true;
+                        }
+                        else if(id == R.id.nav_my_cart){
+                            return true;
+                        }
+                        else if(id == R.id.nav_my_orders){
+                            return true;
+                        }
+                        else if(id == R.id.nav_my_rewards){
+                            return true;
+                        }
+                        else if(id == R.id.nav_my_wishlist){
+                            return true;
+                        }
+                        else if(id == R.id.sign_out){
+                            return true;
+                        }
+                        menuItem.setChecked(true);
+                        //drawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.main_search_icon){
+            Intent intent = new Intent(this,RegisterActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if(id == R.id.main_notification_icon){
+            return true;
+        }
+        else if(id == R.id.main_cart_icon){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 }
-
-
-
